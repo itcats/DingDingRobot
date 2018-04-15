35df62157fa86d533059c6227d76a9d60fbd184f
itcats <364280595@qq.com>
2018-04-15 22:47:55 +0800

    钉钉机器人

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # DingDingRobot
 钉钉机器人webhook
+
+使用方法：
+```php
+ include_once 'vendor/autoload.php';
+ 
+/**
+ * 必填
+ * 钉钉机器人的webhook地址
+ */
+ $web_hook = '';
+ 
+/**
+ * 选填
+ * 需要被通知人的手机号（可在钉钉上查看）
+ */
+ $at_mobiles = array();
+ 
+/**
+ * 选填
+ * 是否全部被@，默认为true
+ */
+ $is_at_all = 'true';
+ 
+ $robot = new Itcats\DingtalkSDK\Dingtalk($web_hook);
+ $txt   = 'hello itcats!';
+ $robot->sendTextMsg($txt);
+```
---
+++ src/Dingtalk.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @Created by PhpStorm
+ * @Author  : itcats(364280595@qq.com)
+ * @Date    :   2018-04-13 15:20
+ */
+
+namespace Itcats\DingtalkSDK;
+
+/**
+ * Class Dingtalk
+ * @package Itcats\DingtalkSDK
+ */
+class Dingtalk
+{
+    /**
+     * 钉钉机器人webhoot
+     * @var string
+     */
+    protected $web_hook = '';
+
+    /**要被at人的电话号码
+     * @var array
+     */
+    protected $at_mobiles = array();
+
+    /**
+     * 是否全部at，true，false
+     * @var string
+     */
+    protected $is_at_all = '';
+
+    public function __construct($web_hook = '', $at_mobiles = array(), $is_at_all = 'true')
+    {
+        if (!empty($web_hook)) {
+            $this->web_hook = $web_hook;
+        }
+        $this->at_mobiles = $at_mobiles;
+        $this->is_at_all  = $is_at_all;
+    }
+
+    /**
+     * 发送机器人消息
+     * @param string $msg
+     * @return mixed|string
+     */
+    public function sendTextMsg($msg = '')
+    {
+        if (empty($msg)) {
+            return 'no msg!';
+        }
+        $data = array(
+            'msgtype' => 'text',
+            'text'    => array('content' => $msg),
+            'at'      => array('atMobiles' => $this->at_mobiles, 'isAtAll' => $this->is_at_all),
+        );
+
+        $result = $this->_sendPost($data);
+        return $result;
+    }
+
+
+    /**
+     * 请求函数
+     * @param $data
+     * @return mixed
+     */
+    private function _sendPost($data)
+    {
+        $ch      = curl_init($this->web_hook);
+        $payload = json_encode($data);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+}
